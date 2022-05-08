49bad8c1be61afa2a5e9f46aa64cc95f17928e5b
Mohamed Shafeeq Usman <mohamedshafeeq.usman@sjsu.edu>
2022-05-07 19:59:51 -0700

    Generate random numbers skeleton

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.util.Random;
+
 class Main{
-    public static void main(String[] args) {
-        System.out.println("Test");
+    public static void main(String[] args) {  
+        int n = 10, range = 100;
+        int randomArray[] = new int[n];
+        Random randNum = new Random();
+        for (int i = 0;i < n; i++){
+            randomArray[i] = randNum.nextInt(range)+1;
+        }
+
+        for (int i: randomArray){
+            System.out.println(i);
+        }
     }       
 }
