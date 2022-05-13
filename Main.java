39504c8fd84778cbf75b9f803eb3aa7723402791
Pranav <pranav@pranav.local>
2022-05-13 14:46:57 -0700

    latest

--- Main.java
+++ Main.java
@@ -3,26 +3,23 @@ import java.util.concurrent.ForkJoinPool;
 
 class Main{
     public static void main(String[] args) {  
-    	int[] sizeArray = {10000000, 13000000, 16000000, 20000000, 25000000};
-        
-        for(int i=0; i<sizeArray.length; i++)
+        int numberList1[] = generateRandom(90000000, 10000000);
+        for(int i=0; i<3; i++)
         {
-            int numberList1[] = generateRandom(sizeArray[i], 10000000);
-            System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  SIZE: "+sizeArray[i]);
-            callThreadedQuicksort(numberList1);
-            callQuicksort(numberList1);
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ \n");
+        callThreadedQuicksort(numberList1);
+        callQuicksort(numberList1);
         }
     }      
 
     public static void callThreadedQuicksort(int[] numbers){
-        
         ForkJoinPool pool = ForkJoinPool.commonPool();
         int len = numbers.length-1;
         long start = System.currentTimeMillis();
         ThreadedQuicksort task = new ThreadedQuicksort(numbers,0,len);
         int res = pool.invoke(task);
         long end = System.currentTimeMillis();
-        System.out.println("Threaded Quicksort time taken: "+(end-start)+" ms");
+        System.out.println("\nThreaded Quicksort Time taken: "+(end-start)+" ms");
         // }
     }
 
@@ -31,7 +28,7 @@ class Main{
         long start = System.currentTimeMillis();
         Quicksort task = new Quicksort(numbers,0,len);
         long end = System.currentTimeMillis();
-        System.out.println("QuickSort time taken: "+(end-start)+" ms");
+        System.out.println("Quicksort Time taken: "+(end-start)+" ms");
     }
 
     public static int[] generateRandom(int size, int range){
